8fbdbf3ad5a74a1b22cb1d01a6fb144e0ffbf4fc
fansi-sifan <sifan1121@gmail.com>
2020-10-18 21:44:53 +0800

    create shanghai district maps

--- load.R
+++ load.R
@@ -32,3 +32,29 @@ response$request
 toJSON(fromJSON(content(response, as = "text")), pretty = TRUE)
 
 
+# Map data ===
+
+# from http://datav.aliyun.com/
+
+library(geojsonsf)
+shanghai <- geojsonsf::geojson_sf(url("https://geo.datav.aliyun.com/areas_v2/bound/310000_full.json"))
+library(ggplot2)
+
+ggplot(shanghai, aes(fill = name))+
+  geom_sf()+
+  theme_classic()
+
+
+# SANDBOX ===
+# https://gadm.org/download_country_v3.html
+
+# raster::getData('GADM',country="CHN",level=1)
+library(sf)
+library(tmap)
+level <- 1 # choose between 0 - 3
+map_url <- paste0("https://biogeo.ucdavis.edu/data/gadm3.6/Rsf/gadm36_CHN_",
+                  level,"_sf.rds")
+
+map_china <- readRDS(url(map_url))
+save(map_china_l3, file = "map_china.rda")
+
