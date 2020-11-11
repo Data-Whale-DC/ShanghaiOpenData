4235d255f12c9aff5c3271661ab01b5b93500cab
fansi-sifan <sifan1121@gmail.com>
2020-11-11 13:12:23 +0800

    new data

--- MapShanghai/app.R
+++ MapShanghai/app.R
@@ -16,13 +16,22 @@ library(DT)
 library(shinythemes)
 
 load("shanghai.rda")
-load("emerg.rda")
+# load("emerg.rda")
+load("data.rda")
 
-map_data <- merge(shanghai, emerg)
+# df <- cleaned$`表2.2 各区土地面积、常住人口及人口密度（2017）` %>% 
+#     rename(name = 地区, area = `行政区划面积（平方公里）`) %>% 
+#     mutate(across(-name, as.numeric))
 
-create_map <- function(col){
-    # col <- enquo(col)
-    map <- ggplot(map_data, aes_string(fill = col, text = "name"))+
+# map_data <- shanghai %>% 
+#     left_join(df, by = "name")
+
+create_map <- function(df, col){
+    #
+    col <- ifelse(col %in% paste0("`",colnames(df)[-1], "`"), 
+                  col, colnames(df)[[2]])
+    
+    map <- ggplot(data = df, aes_string(fill = col, text = "name"))+
         geom_sf()+
         ggthemes::theme_map()+
         theme(text = element_text(family = "Hei"))
@@ -44,7 +53,7 @@ ui <- fluidPage(
     titlePanel(
         fluidRow(
             column(5, img(height = 100, src = "soda-datawhale.png")),
-            column(7, h4("placeholder text to introduce the app, its relationship with the r package, etc.")),
+            column(7, h4("使用说明：\n1. 选择数据表，点击确定\n2. 选择绘制区域热力图的参数，点击确定")),
         ),
         
     ),
@@ -55,24 +64,61 @@ ui <- fluidPage(
     # Sidebar
     sidebarLayout(
         sidebarPanel(
-            selectizeInput("dataset", "选择数据表", choices = "TBD"),
-            selectizeInput("col","选择参数",
-                           choices = colnames(emerg)[-1]),
+            selectizeInput("dataset", "选择数据表", 
+                           choices = names(cleaned)),
+            actionButton("confirmdata", "确定"),
             DT::dataTableOutput("data")
         ),
 
         # Main panel viz
         mainPanel(
-           plotlyOutput("map")
+            
+            selectizeInput("col","选择地图参数",
+                           choices = "请选择参数"),
+            actionButton("confirmcol", "确定"),
+            plotlyOutput("map")
         )
     )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-    output$data <- renderDataTable(emerg %>% select(name, input$col))
+# Define server logic 
+server <- function(input, output, session) {
+    setup <- eventReactive(input$confirmdata, {
+        
+        dt <- cleaned[[input$dataset]]
+        
+        updateSelectizeInput(session, "col", "选择地图参数",
+                             choices = paste0("`",colnames(dt)[-1], "`"), 
+                             selected = colnames(dt)[2])
+        
+        dt
+ 
+    })
+    
+    col <- reactive({
+        col <- input$col
+    })
+    
+    output$data <- renderDataTable({
+        datatable <- setup()
+        DT::datatable(datatable) %>%
+            formatRound(columns = colnames(datatable)[-1], digits = 1)
+    })
+  
+    output$map <- renderPlotly({
+        map <- setup()%>%
+            rename(name = 地区)
 
-    output$map <- renderPlotly(create_map(input$col))
+        map_data <- shanghai %>%
+            left_join(map, by = "name")
+        
+        validate(
+            need(input$col, "请选择地图参数"),
+            need(input$confirmcol, "请点击确定")
+            )
+        
+        create_map(map_data, col())
+        })
 }
 
 # Run the application 
