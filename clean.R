cae43a67f7e6c758e1827bdb9459a43d1a8ddfc7
fansi-sifan <sifan1121@gmail.com>
2020-11-12 17:39:05 +0800

    update indicators

--- clean.R
+++ clean.R
@@ -1,13 +1,6 @@
 library(tidyverse)
 library(tidylog)
 # read 上海统计局 xls. file ---
-all_stat <- list.files(path = "Data", pattern = ".xls", full.names = T)
-data <- all_stat %>% 
-  map(readxl::read_xls) 
-
-# 处理
-paste_name <- paste(df[n, ], name)
-
 clean_stat <- function(df){
   # find which row starts with 全市 or 总计
   n <- grep("全|总",df[,1] %>% pull())-1
@@ -22,27 +15,62 @@ clean_stat <- function(df){
   df[-(1:n), ]
 }
 
-cleaned <- data %>% 
-  map(clean_stat)
+clean_raw <- function(year){
+  all_stat <- list.files(path = paste0("Data/",year), pattern = ".xls", full.names = T)
+  data <- all_stat %>% 
+    map(readxl::read_xls) 
+  
+  # 处理
+  # paste_name <- paste(df[n, ], name)
+  
+  
+  
+  cleaned <- data %>% 
+    map(clean_stat)
+  
+  # dataset names
+  names(cleaned) <- map_chr(1:length(data),~ names(data[[.x]])[[1]])
+  return(cleaned)
+}
 
-# dataset names
-names(cleaned) <- map_chr(1:length(data),~ names(data[[.x]])[[1]])
+cleaned_2017 <- clean_raw(2017)
+cleaned_2012 <- clean_raw(2012)
+
 
 # Manual clean
-cleaned %>% 
-  openxlsx::write.xlsx(file = "Data/census.xlsx")
+cleaned_2017 %>% 
+  openxlsx::write.xlsx(file = "Data/census_2017.xlsx")
+
+cleaned_2012 %>% 
+  openxlsx::write.xlsx(file = "Data/census_2012.xlsx")
+
 
 library(readxl)
 
-list <- excel_sheets("Data/census.xlsx")
-cleaned <- list %>% 
-  map(read_xlsx, path = "Data/census.xlsx")
-names(cleaned) <- list
+clean_final <- function(year){
+  list <- excel_sheets(paste0("Data/census_",year,".xlsx"))
+  cleaned <- list %>% 
+    map(read_xlsx, path = paste0("Data/census_",year,".xlsx"))
+  names(cleaned) <- list
+  
+  cleaned <- cleaned %>% 
+    map(~mutate(.x, across(c(-地区), 
+                           ~as.numeric(gsub("…", "", .x)))))
+}
 
-cleaned <- cleaned %>% 
-  map(~mutate(.x, across(c(-地区), 
-                         ~as.numeric(gsub("…", "", .x)))))
+merge_zhabei <- function(df){
+  df %>% 
+    mutate(地区 = ifelse(地区 == "闸北区", "静安区", 地区),
+             地区 = ifelse(地区 == "崇明县", "崇明区", 地区)) %>% 
+    group_by(地区) %>% 
+    summarise(across(where(is.numeric), sum, na.rm = T))
+}
+
+cleaned <- clean_final(2017)
+cleaned_2012 <- clean_final(2012) %>% 
+  map(merge_zhabei)
 
 # final
 save(cleaned, file = "MapShanghai/data.rda")
+save(cleaned_2012, file = "MapShanghai/data_2012.rda")
 
--- do.R
+++ do.R
@@ -1,76 +1,136 @@
 # SETUP, create indicators for dashboard
 library(tidyverse)
 library(tidylog)
-# 1. fix duplicated names
-cleaned %>%
-  map(colnames)
-
 library(magrittr)
-cleaned$`表2.7 各区户籍老年人口年龄构成（2017）` %<>%
-  rename(`老年合计（单位：万人）` = `合计（单位：万人）`)
 
-cleaned$`表11.6 各区各类房屋分布情况（2017）` %<>%
-  select(-非居住房屋)
+cleaned_2012 %>%
+  map(colnames)
+# 1. fix duplicated names
+fix_names_2017 <- function(df){
 
-cleaned$`表14.3 各区建筑业主要指标（2017）` %<>%
-  rename(`建筑业年末从业人员（万人）` = `年末从业人员（万人）`)
+  df$`表2.7 各区户籍老年人口年龄构成（2017）` %<>%
+    rename(`老年合计（单位：万人）` = `合计（单位：万人）`)
+  
+  df$`表11.6 各区各类房屋分布情况（2017）` %<>%
+    select(-非居住房屋)
+  
+  df$`表14.3 各区建筑业主要指标（2017）` %<>%
+    rename(`建筑业年末从业人员（万人）` = `年末从业人员（万人）`)
+  
+  df$`表13.2 各区工业企业主要指标（2017）` %<>%
+    rename(`工业年末从业人员（万人）` = `年末从业人员（万人）`)
+  
+  df$`表20.15 各区普通小学基本情况（2017）` %<>%
+    rename_if(is.numeric, ~ paste0("小学", .x))
+  
+  df$`表20.13 各区普通中学基本情况（2017）` %<>%
+    rename_if(is.numeric, ~ paste0("中学", .x))
+  
+  return(df)
+}
 
-cleaned$`表13.2 各区工业企业主要指标（2017）` %>%
-  rename(`工业年末从业人员（万人）` = `年末从业人员（万人）`)
-
-cleaned$`表20.15 各区普通小学基本情况（2017）` %<>%
-  rename_if(is.numeric, ~ paste0("小学", .x))
-
-cleaned$`表20.13 各区普通中学基本情况（2017）` %<>%
-  rename_if(is.numeric, ~ paste0("中学", .x))
-
-master <- plyr::join_all(cleaned, by = "地区")
-
-population <- master %>%
-  transmute(
-    地区 = 地区,
-    `外来人口占常住人口比例(%)` = `其中外来人口` / `年末常住人口（万人）` * 100,
-    `净迁入人口占常住人口比例(%)` = (`2017市外迁入` - `2017迁往市外`) / `年末常住人口（万人）` / 100,
-    `老龄人口(60岁及以上）占常住人口比例(%)` = `60岁及以上` / `年末常住人口（万人）` * 100
-  ) %>%
-  pivot_longer(-地区) %>%
-  mutate(类别 = "城市人口")
-
-housing <- master %>%
-  transmute(
-    地区 = 地区,
-    `人均居住房屋面积（平方米/人）` = `其中居住房屋` / `年末常住人口（万人）`,
-    `旧式里弄占住房面积比例(%)` = `旧式里弄` / `其中居住房屋` * 100,
-    `人均绿地面积（公顷/人）` = `绿地面积（公顷）` / `年末常住人口（万人）`
-  ) %>%
-  pivot_longer(-地区) %>%
-  mutate(类别 = "居住环境")
-
-economics <- master %>%
-  transmute(
-    地区 = 地区,
-    `工业生产力（元/人）` = `工业总产值（亿元）` / `年末从业人员（万人）` * 10000,
-    `建筑业生产力（元/人）` = `总产值（亿元）` / `建筑业年末从业人员（万人）` * 10000,
-    `商用建筑物占全部房屋比例（%）` = (`非居住房屋` - `学校`) / `全部房屋合计（单位：万平方米）` * 100
-  ) %>%
-  pivot_longer(-地区) %>%
-  mutate(类别 = "经济发展")
-
-education <- master %>%
-  transmute(
-    地区 = 地区,
-    `专任教师: 小学在校学生 =（1：？）` = `小学在校学生（人）` / `小学其中#专任教师`,
-    `医护人员: 常住人口 =（1：？）` = `年末常住人口（万人）` * 10000 / `卫生技术人员（人）`,
-    `床位数: 老年人口 =（1：？）` = `60岁及以上` * 10000 / `床位数（张）`
-  ) %>%
-  pivot_longer(-地区) %>%
-  mutate(类别 = "教育卫生")
+fix_names_2012 <- function(df){
+  df %>%
+    map(colnames)
+  
+  df$`表2.7 各区、县户籍老年人口年龄构成（2012）` %<>%
+    rename(`老年合计（单位：万人）` = `合计（单位：万人）`)
+  
+  df$`表11.6 各区、县各类房屋分布情况（2012）` %<>%
+    select(-非居住房屋)
+  
+  df$`表11.17 各区、县绿化面积（2012）` %<>% 
+    rename(`绿地面积（公顷）` = `城市绿地面积（公顷）`)
+  
+  df$`表14.3 各区、县建筑业主要指标（2012）` %<>%
+    rename(`建筑业年末从业人员（万人）` = `年末从业人员（万人）`)
+  cleaned_2012$`表14.3 各区、县建筑业主要指标（2012）` %<>%
+    mutate(`总产值（亿元）` = `总产值（亿元）` * 1.1)
+  
+  df$`表13.2 各区、县工业企业主要指标（2012）` %<>%
+    mutate(`工业总产值（亿元）` = `工业总产值（亿元）`* 1.1)
+  df$`表13.2 各区、县工业企业主要指标（2012）` %<>%
+    rename(`工业年末从业人员（万人）` = `从业人员（万人）`)
+  
+  df$`表20.15 各区、县普通小学基本情况（2012）` %<>%
+    rename_if(is.numeric, ~ paste0("小学", .x))
+  df$`表20.13 各区、县普通中学基本情况（2012）`%<>%
+    rename_if(is.numeric, ~ paste0("中学", .x))
+  
+  return(df)
+}
 
 
-indicators <- bind_rows(population, housing, economics, education) %>% view()
+create_indicators <- function(master){
+  population <- master %>%
+    transmute(
+      地区 = 地区,
+      年份 = year,
+      `外来人口占常住人口比例(%)` = `其中外来人口` / `年末常住人口（万人）` * 100,
+      `净迁入人口占常住人口比例(%)` = (`市外迁入` - `迁往市外`) / `年末常住人口（万人）` / 100,
+      `老龄人口(60岁及以上）占常住人口比例(%)` = `60岁及以上` / `年末常住人口（万人）` * 100
+    ) %>%
+    pivot_longer(-c("地区","年份")) %>%
+    mutate(类别 = "城市人口")
+  
+  housing <- master %>%
+    transmute(
+      地区 = 地区,
+      年份 = year,
+      `人均居住房屋面积（平方米/人）` = `其中居住房屋` / `年末常住人口（万人）`,
+      `旧式里弄占住房面积比例(%)` = `旧式里弄` / `其中居住房屋` * 100,
+      `人均绿地面积（公顷/人）` = `绿地面积（公顷）` / `年末常住人口（万人）`
+    ) %>%
+    pivot_longer(-c("地区","年份")) %>%
+    mutate(类别 = "居住环境")
+  
+  economics <- master %>%
+    transmute(
+      地区 = 地区,
+      年份 = year,
+      `工业生产力（元/人）` = `工业总产值（亿元）` / `工业年末从业人员（万人）` * 10000,
+      `建筑业生产力（元/人）` = `总产值（亿元）` / `建筑业年末从业人员（万人）` * 10000,
+      `商用建筑物占全部房屋比例（%）` = (`非居住房屋` - `学校`) / (`其中居住房屋` + `非居住房屋`) * 100
+    ) %>%
+    pivot_longer(-c("地区","年份")) %>%
+    mutate(类别 = "经济发展")
+  
+  education <- master %>%
+    transmute(
+      地区 = 地区,
+      年份 = year,
+      `专任教师: 小学在校学生 =（1：？）` = `小学在校学生（人）` / `小学其中#专任教师`,
+      `医护人员: 常住人口 =（1：？）` = `年末常住人口（万人）` * 10000 / `卫生技术人员（人）`,
+      `床位数: 老年人口 =（1：？）` = `60岁及以上` * 10000 / `床位数（张）`
+    ) %>%
+    pivot_longer(-c("地区","年份")) %>%
+    mutate(类别 = "教育卫生")
+  
+  bind_rows(population, housing, economics, education) 
+  
+}
+
+master <- bind_rows(
+  plyr::join_all(fix_names_2017(cleaned), by = "地区") %>%
+    mutate(year = "2017"),
+  
+  plyr::join_all(fix_names_2012(cleaned_2012), by = "地区") %>% 
+    mutate(year = "2012")
+  ) 
+
+indicators <- master %>% 
+  create_indicators()
+
 save(indicators, file = "MapShanghai/indicators.rda")
 
+# chart
 library(tidytext)
+# install.packages("extrafont")
+# library(extrafont)
+# font_import()
+# 
+# # 显示字体信息
+# fonts()
 
 show_plot <- function(type) {
   indicators %>%
@@ -81,23 +141,27 @@ show_plot <- function(type) {
       aes(
         x = 地区,
         y = value,
-        fill = 类别,
+        fill = 年份,
         color = 全市
       )
     ) +
-    geom_col() +
+    geom_col(position = "dodge") +
     scale_x_reordered() +
     scale_color_manual(values = c("#f1a340", "#f7f7f7")) +
     scale_fill_manual(values = c("#a6cee3", "#1f78b4", "#b2df8a", "#33a02c")) +
     facet_wrap(~name, scales = "free") +
     coord_flip() +
+    guides(color = F)+
     labs(x = "", y = "") +
     theme_classic() +
     theme(
-      text = element_text(family = "Hei"),
-      legend.position = "none"
+      text = element_text(family = "SimHei")
     )
 }
 
+show_plot("经济发展")
 
-plotly::ggplotly(show_plot("教育卫生"))
+library(plotly)
+ggplotly(show_plot("教育卫生"), tooltip = c("地区", "年份","value")) %>% 
+  hide_legend
+  
